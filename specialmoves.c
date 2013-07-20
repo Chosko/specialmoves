0b984d075295c23262d6beb535e7b2bddbb231d0
Chosko <ruben.caliandro@gmail.com>
2013-07-20 05:53:53 +0200

    lookup array created

--- specialmoves.c
+++ specialmoves.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFSIZE 1024 * 1024
+#define BUFSIZE 1024 * 1024 
 #define MOVLEN 128
+#define NAMELEN 32
+#define LULEN 128
 #define LF 10
 #define COLON 58
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+
+typedef struct
+{
+	unsigned char name[NAMELEN];	//the name of the move
+	unsigned char seq[MOVLEN];	//the key sequence of the move
+	unsigned char *status;
+	int length;			//the length of the move
+	int count;			//counter for lookup search
+} move;
 
 FILE *infile;					//input file
 FILE *outfile;					//output file
@@ -22,6 +33,10 @@ unsigned char *inmax;			//input buffer max ptr value
 unsigned char *outbuf;			//output buffer
 unsigned char *outcur;			//output buffer cursor
 unsigned char *outmax;			//output buffer max ptr value
+
+move lu[LULEN];					//lookup array
+int searches[LULEN];			//search indices
+int searchcount = 0;			//current search count
 
 unsigned char cbuf[MOVLEN];		//circular buffer
 unsigned char *cleft;			//left cursor of cbuf
@@ -87,12 +102,19 @@ int main(int argc, char *argv[]){
 	register int i;
 	register unsigned char peek;
 	register int status = 1;
+	unsigned char tmp1 = 0;
+	unsigned char tmp2 = 0;
+	unsigned char curmove = 0;
+	
 	
 	while(inleft)
-	{
+	{	
+		//reads from file
 		byte_to_read = MIN(inleft, bufsize);
 		inread = fread(inbuf, 1, byte_to_read, infile);
 		inleft -= inread;
+		
+		//while input buffer not empty
 		for (i = 0; i < inread; i++)
 		{
 			peek = inbuf[i];
@@ -109,24 +131,56 @@ int main(int argc, char *argv[]){
 				}
 			}
 			
-			//reading left side of declaration
+			//reading right side of declaration
 			else if(status == 1)
 			{
 				if(peek == LF)
+				{
 					status = 2;
+					lu[curmove].seq[tmp2] = 0;
+					lu[curmove].length = tmp2;
+				}
+				else
+				{	
+					if(tmp1)
+					{
+						tmp1=0;
+						tmp2=0;
+						curmove = peek;
+						while(lu[curmove].status)
+							curmove++;
+						lu[curmove].status = "taken";
+						strcpy(lu[curmove].name, lu[0].name);
+					}
+					lu[curmove].seq[tmp2] = peek;
+					tmp2++;
+				}
 			}
 			
-			//reading right side of declaration
+			//reading left side of declaration
 			else
 			{
 				if(peek == COLON)
+				{
 					status = 1;
+					lu[0].name[tmp1] = 0;
+					tmp2 = 0;
+				}
 				else if(peek == LF)
 					status = 0;
+				else
+				{
+					lu[0].name[tmp1] = peek;
+					tmp1++;
+				}
 			}
 		}
 	}
 	
 	flushbuf();
+//	for(i=LULEN; i--; )
+//	{
+//		printf("%d - %s:%s\n", i, lu[i].name, lu[i].seq);
+//	}
 	return 0;
 }
